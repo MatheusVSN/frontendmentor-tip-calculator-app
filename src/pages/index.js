f28cd80b6b4ee1948dcf1a396ddead912b2e4e53
Matheus <125493278+MatheusVSN@users.noreply.github.com>
2023-03-30 17:11:15 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
 
 import IconPerson from "../images/icon-person.svg";
 
 export default function Home() {
   const Ratings = [5, 10, 15, 25, 50];
+  const [rating, setRating] = React.useState(0);
+  const [customRating, setCustom] = React.useState(0);
+
+  function onMouseButton1Click(givenRating) {
+    setRating(givenRating);
+  }
+
+  function inputChanged() {
+    setRating(0);
+  }
+
+  function enterPressed(event) {
+    if (event.key == "Enter") {
+      setCustom(event.target.value);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -40,12 +58,22 @@ export default function Home() {
             <p>Select Tip %</p>
             <div className="selector-container">
               {Ratings.map((index) => (
-                <button className="rating-button" key={index}>{`${index}%`}</button>
+                <button
+                  className={`rating-button ${
+                    rating == index ? "rating-active" : ""
+                  }`}
+                  onClick={() => onMouseButton1Click(index)}
+                  x
+                  key={index}
+                >{`${index}%`}</button>
               ))}
               <input
                 type="text"
                 className="tip-input"
                 placeholder="Custom"
+                onClick={inputChanged}
+                onChange={inputChanged}
+                onKeyDown={enterPressed}
               ></input>
             </div>
           </div>
@@ -67,14 +95,18 @@ export default function Home() {
           <ul>
             <li>
               <div className="price-container-1 float-left">
-                <p className="text-1">Tip Ammount <br/> <b className="text-2">/ Person</b></p>
+                <p className="text-1">
+                  Tip Ammount <br /> <b className="text-2">/ Person</b>
+                </p>
               </div>
 
               <p className="result float-right">$0.00</p>
             </li>
             <li>
               <div className="price-container-1 float-left">
-                <p className="text-1">Total <br/> <b className="text-2">/ Person</b></p>
+                <p className="text-1">
+                  Total <br /> <b className="text-2">/ Person</b>
+                </p>
               </div>
 
               <p className="result float-right">$0.00</p>
@@ -86,4 +118,8 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+function GenerateButton() {
+  return;
 }
